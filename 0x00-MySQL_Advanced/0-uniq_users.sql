0668b2f7fa8fb80552b13a911161d37f3c6b6000
= <=>
2023-07-12 17:17:30 +0000

    inclusion of the primary key as the id field

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,8 +1,14 @@
---This is a script file that creates a users table
-CREATE TABLE IF NOT EXISTS users(
+-- This is a script file that creates a users table
+-- id, integer, never null, auto increment and primary key
+-- email, string (255 characters), never null and unique
+-- name, string (255 characters)
+
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users(
 	id INT NOT NULL AUTO_INCREMENT,
-	email varchar(255) NOT NULL UNIQUE,
-	name varchar(255),
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255),
 	PRIMARY KEY(id)
 	);
 
