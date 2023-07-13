2ac60e4e80f95cbb12cc2b7bf5ed373bc1bbe378
= <=>
2023-07-13 15:19:07 +0000

    task 3 solution

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,6 @@
+-- This is an sql script that lists all bands with Glam rock as their main style, ranked by their longevity
+
+SELECT band_name, ifnull(split, 2020)-ifnull(formed, 0) AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC;
