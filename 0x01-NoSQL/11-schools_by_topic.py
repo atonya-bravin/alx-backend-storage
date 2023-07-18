aec4e80ed1e3a926fa6832a9ec5f7ed08aaaf81d
= <=>
2023-07-18 16:27:18 +0000

    Contains a method that returns a list of schools having a specific topic

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+This is a script that contains a method that
+returns the list of school having a specific topic
+"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    This method returns the list of school having a specific topic
+    """
+
+    return monfo_collection.find({"topics": topic})
